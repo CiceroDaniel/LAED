b416805d40a15360b8943d9a47e585cf1a57921e
Cícero Daniel Barbosa dos Santos <92804925+CiceroDaniel@users.noreply.github.com>
2026-02-19 14:28:53 +0000

    BUBBLE SORT BASICO

--- main.c
+++ main.c
@@ -1,55 +1,46 @@
-int comparacoes = 0;  // Contador global
-int trocas = 0;       // Contador global
+int comparacoes = 0; // Contador global
+int trocas = 0;      // Contador global
 
 void bubbleSort(int v[], int n) {
-    // TODO: Implementar aqui
-    // Incrementar comparacoes++ a cada comparação
-    // Incrementar trocas++ a cada troca
+  // TODO: Implementar aqui
+  // Incrementar comparacoes++ a cada comparação
+  // Incrementar trocas++ a cada troca
 
   int i, j, aux;
 
-//BUBBLE SORT SIMPLES
-    
-    /*for(i = 0; i < n-1; i ++){
+  // BUBBLE SORT SIMPLES
 
-        for(j = 0; j < n-i-1; j++){
+  for (i = 0; i < n - 1; i++) {
 
-            if(v[j] > v[j+1]){
-                aux = v[j];
-                v[j] = v[j+1];
-                v[j] = aux;
-            }
-        
-        }
-    
-    }*/
+    for (j = 0; j < n - i - 1; j++) {
 
-
-  
-  
-
-
-  
+      if (v[j] > v[j + 1]) {
+        aux = v[j];
+        v[j] = v[j + 1];
+        v[j] = aux;
+      }
+    }
+  }
 }
 
 void imprimirVetor(int v[], int n) {
-    for(int i = 0; i < n; i++)
-        printf("%d ", v[i]);
-    printf("\n");
+  for (int i = 0; i < n; i++)
+    printf("%d ", v[i]);
+  printf("\n");
 }
 
 int main() {
-    int dados[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 7;
+  int dados[] = {64, 34, 25, 12, 22, 11, 90};
+  int n = 7;
 
-    printf("Vetor original: ");
-    imprimirVetor(dados, n);
+  printf("Vetor original: ");
+  imprimirVetor(dados, n);
 
-    bubbleSort(dados, n);
+  bubbleSort(dados, n);
 
-    printf("Vetor ordenado: ");
-    imprimirVetor(dados, n);
-    printf("Comparações: %d | Trocas: %d\n", comparacoes, trocas);
+  printf("Vetor ordenado: ");
+  imprimirVetor(dados, n);
+  printf("Comparações: %d | Trocas: %d\n", comparacoes, trocas);
 
-    return 0;
+  return 0;
 }
