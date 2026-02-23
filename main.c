5be3ecfadf45bedb807d0fdf1e4974f325fe0b11
Cícero Daniel Barbosa dos Santos <92804925+CiceroDaniel@users.noreply.github.com>
2026-02-23 12:28:51 +0000

    Questão 3 da tarefa semanal de BUBBLE SORT

--- main.c
+++ main.c
@@ -1,84 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-void bubble_sem_otimizacao(int dados[], int n, long *comparacoes, long *trocas){
-  *comparacoes = 0, *trocas = 0;
-  for(int i = 0; i < n -1; i++){
-    for(int j = 0; j < n - i - 1; j++){
-      (*comparacoes)++;
-      if(dados[j] > dados[j+1]){
-        int aux = dados[j];
-        dados[j] = dados[j + 1];
-        dados[j + 1] = aux;
-        (*trocas)++;
-      }
-    }
-  }
-}
+//QUESTÃO 3, LETRA A
 
-void bubble_otimizado(int dados[], int n, long *comparacoes, long *trocas){
+void bubble_notas(float notas[], int n, long *comparacoes, long *trocas){
   *comparacoes = 0, *trocas = 0;
   for(int i = 0; i < n-1; i++){
-    int trocou = 0;
     for(int j = 0; j < n-i-1; j++){
       (*comparacoes)++;
-      if(dados[j] > dados[j+1]){
-        int aux = dados[j];
-        dados[j] = dados[j+1];
-        dados[j+1] = aux;
+      if(notas[j] < notas[j+1]){
+        float aux = notas[j];
+        notas[j] = notas[j+1];
+        notas[j+1] = aux;
         (*trocas)++;
-        trocou = 1;
       }
     }
-    if (trocou == 0){
-      break;
-    }
   }
-}
-
-void testar(char* nome, int dados[], int n){
-  printf("\n------- %s -------\n", nome);
-
-  // SEM otimização
-  int copia1[20]; 
-  for(int k = 0; k<n; k++) copia1[k] = dados[k];
-  long comps1, trocas1;
-  bubble_sem_otimizacao(copia1, n, &comps1, &trocas1);  
-  printf("Sem otimização: %ld comparações, %ld trocas\n", comps1, trocas1);
-
-  // COM otimização
-  int copia2[20]; 
-  for(int k = 0; k<n; k++) copia2[k] = dados[k];
-  long comps2, trocas2;
-  bubble_otimizado(copia2, n, &comps2, &trocas2);  
-  printf("Com otimização: %ld comparações, %ld trocas\n", comps2, trocas2);
 }
 
 int main(){
-  int n = 20;
-
-  // 1. Vetor ORDENADO
-  int ordenado[20];
-  for(int i = 0; i < n; i++){
-    ordenado[i] = i;
+  int n = 12;
+  float notas[12] = {725.5, 892.0, 650.0, 945.5, 780.2, 910.0, 
+                     675.8, 823.4, 955.0, 712.3, 880.7, 698.1};
+  printf("--Notas antes da ordenação--\n");
+  for(int i = 0;i <n;i++){
+    printf("%.1f ", notas[i]);
   }
-  testar("Vetor ordenado", ordenado, n);
 
-  // 2. Vetor REVERSO
-  int reverso[20];
-  for(int i = 0; i < n; i++){
-    reverso[i] = n - 1 - i;  
+  long comparacoes, trocas;
+  bubble_notas(notas, n, &comparacoes, &trocas);
+
+  printf("notas depois da ordenação\n");
+
+  for(int i =0; i < n;i++){
+    printf("%.1f ", notas[i]);
   }
-  testar("Vetor reverso", reverso, n);
 
-  // 3. Vetor ALEATÓRIO
-  int aleatorio[20];  
-  srand(time(NULL));
-  for(int i = 0; i < n; i++){
-    aleatorio[i] = rand() % 100;
+  printf("\n");
+
+
+  printf("\n TOP 3 notas: %.1f, %.1f, %.1f\n", notas[0], notas[1], notas[2]);
+
+  float media = 0;
+  for(int i = 0; i < n;i++){
+    media += notas[i];
   }
-  testar("Vetor aleatorio", aleatorio, n);
 
-  return 0;
+  media /= n;
+
+  printf("Média das notas: %.1f\n", media);
+  printf("Número de comparações: %ld\n", comparacoes);
+  printf("Número de trocas: %ld\n", trocas);
 }
